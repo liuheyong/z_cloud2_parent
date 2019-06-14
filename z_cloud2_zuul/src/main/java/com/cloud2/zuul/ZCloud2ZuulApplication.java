296b4efaefdcbde45c3406198346aaa4109ef9af
liuheyong <17682347237@163.com>
2019-06-14 13:53:25 +0800

    commit

---
+++ z_cloud2_zuul/src/main/java/com/cloud2/zuul/ZCloud2ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.cloud2.zuul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZCloud2ZuulApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZCloud2ZuulApplication.class).web(true).run(args);
+    }
+
+}
